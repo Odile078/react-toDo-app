d37c5c943366bcd5f6134c5b1ce646c0be361ce2
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-21 18:32:57 +0200

    chore: set up React js and Tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-5xl">
+        <h1 className="text-red-700">To do app</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
